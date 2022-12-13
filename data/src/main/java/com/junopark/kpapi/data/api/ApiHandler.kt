bdeeba1d100058e7069989dc8694a56836f18bcd
palevominimale <badtri.orchestra@gmail.com>
2022-12-13 16:40:38 +0100

    added interceptor

--- data/src/main/java/com/junopark/kpapi/data/api/ApiHandler.kt
+++ data/src/main/java/com/junopark/kpapi/data/api/ApiHandler.kt
@@ -2,13 +2,14 @@
 
 package com.junopark.kpapi.data.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.HttpException
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object ApiHandler {
+class ApiHandler() {
 
     private suspend fun <T: Any> handleApi(
         execute: () -> Response<T>
@@ -28,8 +29,19 @@ object ApiHandler {
         }
     }
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor {
+            val request = it.request().newBuilder()
+                .addHeader("X-API-KEY", "c9c408e6-3675-408b-9a6a-4027f534b2a8")
+                .addHeader("Content-Type", "application/json")
+                .build()
+            it.proceed(request)
+        }
+        .build()
+
     private val retrofit: ApiRequest by lazy {
         Retrofit.Builder()
+            .client(okHttpClient)
             .baseUrl(ApiRequest.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
@@ -43,6 +55,5 @@ object ApiHandler {
             is ApiResult.ApiException -> ApiResult.ApiException(e = res.e)
         }
     }
-
 
 }
