791af9ba173d8f8982f428665053417f6a3179f5
palevominimale <badtri.orchestra@gmail.com>
2022-12-13 18:45:33 +0100

    fixed dependencies and versions

--- app/src/main/java/com/junopark/kpapi/app/di/dataDi.kt
+++ app/src/main/java/com/junopark/kpapi/app/di/dataDi.kt
@@ -1,7 +1,11 @@
 package com.junopark.kpapi.app.di
 
+import com.junopark.kpapi.data.api.ApiRepoImpl
+import com.junopark.kpapi.domain.interfaces.ApiRepo
 import org.koin.dsl.module
 
 val dataDi = module {
-
+    single <ApiRepo> {
+        ApiRepoImpl()
+    }
 }
