65812e8099fe0951450f149adf9c7b1cd129fe5d
LeskovBoris <leskov.2201@yandex.ru>
2022-12-14 21:51:56 +0300

    add database

---
+++ data/src/main/java/com/junopark/kpapi/data/api/database/FavsFilmsDBRepoImpl.kt
@@ -0,0 +1,61 @@
+package com.junopark.kpapi.data.api.database
+
+import android.content.Context
+import androidx.room.Room
+import com.junopark.kpapi.domain.interfaces.FavsFilmsDBRepo
+import com.junopark.kpapi.domain.models.ApiResult
+import com.junopark.kpapi.domain.models.RoomResult
+import com.junopark.kpapi.entities.films.FilmItemMini
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class FavsFilmsDBRepoImpl(context: Context) : FavsFilmsDBRepo {
+
+    private val roomState = MutableStateFlow<RoomResult>(RoomResult.Success(FilmItemMini(null)))
+    override val state: StateFlow<RoomResult> get() = roomState
+
+    private val db:FavsFilmsDB = Room.databaseBuilder(
+        context.applicationContext,
+        FavsFilmsDB::class.java,
+        "Films.db"
+    ).build()
+
+    private val filmsDAO = db.favsFilmsDAO()
+
+
+    override suspend fun getFilms() {
+        roomState.emit(try {
+            val result = filmsDAO.getFilms()
+            if(result != null) RoomResult.Success(result) else RoomResult.Empty
+        } catch (e:Throwable){
+            RoomResult.Error(e)
+        })
+    }
+
+    override suspend fun getFilm(id: Int?) {
+        roomState.emit(try {
+            val result = filmsDAO.getFilm(id)
+            if(result != null) RoomResult.Success(result) else RoomResult.Empty
+        } catch (e:Throwable){
+            RoomResult.Error(e)
+        })
+    }
+
+    override suspend fun addFilm(filmItemMini: FilmItemMini) {
+        roomState.emit(try {
+            val result = filmsDAO.addFilm(filmItemMini) as FilmItemMini
+            if(result != null) RoomResult.Success(result) else RoomResult.Empty
+        } catch (e:Throwable){
+            RoomResult.Error(e)
+        })
+    }
+
+    override suspend fun RemoveFilm(filmItemMini: FilmItemMini) {
+        roomState.emit(try {
+            val result = filmsDAO.removeFilm(filmItemMini) as FilmItemMini
+            if(result != null) RoomResult.Success(result) else RoomResult.Empty
+        } catch (e:Throwable){
+            RoomResult.Error(e)
+        })
+    }
+}
---
+++ domain/src/main/java/com/junopark/kpapi/domain/usecases/RoomUseCase.kt
@@ -0,0 +1,16 @@
+package com.junopark.kpapi.domain.usecases
+
+import com.junopark.kpapi.domain.interfaces.FavsFilmsDBRepo
+import com.junopark.kpapi.entities.films.FilmItemMini
+
+class RoomUseCase(
+    private val favsFilms:FavsFilmsDBRepo
+) {
+
+    suspend fun getFilms() = favsFilms.getFilms()
+    suspend fun getFilm(id:Int?) = favsFilms.getFilm(id)
+    suspend fun addFilm(filmItemMini:FilmItemMini) = favsFilms.addFilm(filmItemMini)
+    suspend fun removeFilm(filmItemMini:FilmItemMini) = favsFilms.RemoveFilm(filmItemMini)
+
+
+}
