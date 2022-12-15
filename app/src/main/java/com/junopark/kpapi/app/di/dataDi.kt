0199eacad15025e40534d3bd36d8d5e3b8f8ed4f
palevominimale <badtri.orchestra@gmail.com>
2022-12-15 12:41:12 +0100

    added icon added screen for single item

--- app/src/main/java/com/junopark/kpapi/app/di/dataDi.kt
+++ app/src/main/java/com/junopark/kpapi/app/di/dataDi.kt
@@ -1,11 +1,27 @@
 package com.junopark.kpapi.app.di
 
 import com.junopark.kpapi.data.repos.ApiRepoImpl
+import com.junopark.kpapi.data.repos.FavsFilmsDBRepoImpl
 import com.junopark.kpapi.domain.interfaces.ApiRepo
+import com.junopark.kpapi.domain.interfaces.FavsFilmsDBRepo
+import com.junopark.kpapi.domain.usecases.RoomUseCase
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val dataDi = module {
     single <ApiRepo> {
         ApiRepoImpl()
+    }
+
+    single {
+        RoomUseCase(
+            favsFilms = get()
+        )
+    }
+
+    single <FavsFilmsDBRepo> {
+        FavsFilmsDBRepoImpl(
+            context = androidContext()
+        )
     }
 }
